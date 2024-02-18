fcac9c8f75595bed65bbcf97c997a186529893e7
n0l1t <n0rthl1ght@yandex.ru>
2024-02-19 01:10:22 +0300

    added functions for initializing the array for task 2 and misc updates

--- firstAtt/task1.py
+++ firstAtt/task1.py
@@ -5,23 +5,23 @@
 # ({ 5, 3, 1, 2, 3, 3, 7, 4, 8 }, { 5, 3, 7, 3, 7, 2, 2, 8, 7 }) → { 5, 3, 3, 2, 2, 3, 3, 3, 3, 7, 7, 7, 8 }
 
 def listcreator(firstList, secondList):
-    outputFile = open('firstAtt/results/outputTask1', 'w+')
+    outputFile = open('/Users/n0rthl1ght/Desktop/вгу/pyhton/firstAtt/results/outputTask1', 'w+')
     i = 0
     j = 0
     while i < len(firstList):
         while j < len(secondList):
             if (firstList[i] == secondList[j]):
-                outputFile.write(str(secondList[j]))
+                outputFile.write(str(secondList[j]) + ' ')
             j += 1
         i += 1
         j = 0
-    outputFile.close
+    outputFile.close()
 
 
 fList = []
 sList = []
 
-with open('firstAtt/input/inputTask1') as file:
+with open('/Users/n0rthl1ght/Desktop/вгу/pyhton/firstAtt/input/inputTask1') as file:
     for line in file:
         for x in line.split():
             fList.append(int(x))
--- firstAtt/task2.py
+++ firstAtt/task2.py
@@ -1,3 +1,25 @@
-#25
-#Написать функцию, которая создаст новый двумерный массив, «повернутый» относительно переданного на 90°.
+# 25
+# Написать функцию, которая создаст новый двумерный массив, «повернутый» относительно переданного на 90°.
 
+def arrayreversal(array):
+    outputFile = open('/Users/n0rthl1ght/Desktop/вгу/pyhton/firstAtt/results/outputTask2', 'w+')
+    outputFile.close()
+
+
+def getarraysize_lines():
+    with open('/Users/n0rthl1ght/Desktop/вгу/pyhton/firstAtt/input/inputTask2') as file:
+        line = sum(1 for line in file)
+    return line
+
+
+def getarraysize_columns():
+    with open('/Users/n0rthl1ght/Desktop/вгу/pyhton/firstAtt/input/inputTask2') as file:
+        for line in file:
+            column = ([int(x) for x in line.split()])
+            break
+    return len(column)
+
+
+arr = [[0] * getarraysize_columns()] * getarraysize_lines()
+
+arrayreversal(arr)
