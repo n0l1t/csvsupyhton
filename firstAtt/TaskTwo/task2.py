969f9ac5c86ca691d76551d01055839d9a15d79f
n0l1t <n0rthl1ght@yandex.ru>
2024-02-19 02:26:01 +0300

    fully completed task 2

--- firstAtt/TaskTwo/task2.py
+++ firstAtt/TaskTwo/task2.py
@@ -1,16 +1,29 @@
 # 25
 # Написать функцию, которая создаст новый двумерный массив, «повернутый» относительно переданного на 90°.
 from links import tTwo_infile, tTwo_outfile
-from getarraysize import columns, lines
+from utils import columns, lines
 
 
 def arrayreversal(array):
+    ax1 = lines()
+    ax2 = columns()
     outputFile = open(tTwo_outfile, 'w+')
+    resultArray = list(map(list, zip(*array[::-1])))
+
+    for i in range (ax2):
+        if i > 0:
+            outputFile.write('\n')
+        for j in range (ax1):
+            outputFile.write(str(resultArray[i][j]) + ' ')
     outputFile.close()
 
 
-arr = [[0] * columns()] * lines()
-
-print(arr)
+with open(tTwo_infile, 'r') as file:
+    ax1 = lines()
+    ax2 = columns()
+    arr = file.readlines()
+    for i in range(ax1):
+        arr[i] = list(map(int, (arr[i].rstrip().split(' ')[:ax2])))
+    arr = arr[:ax1]
 
 arrayreversal(arr)
